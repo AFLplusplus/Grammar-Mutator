fbf899ccc08564a6f417ac460db51eed1a680f3b
h1994st <h1994st@gmail.com>
2020-07-03 18:48:32 -0400

    Add test case for random mutation
    Signed-off-by: h1994st <h1994st@gmail.com>

--- src/tree_mutation.c
+++ src/tree_mutation.c
@@ -2,6 +2,20 @@
 #include "json_c_fuzz.h"
 
 node_t *_pick_node(node_t *root) {
+  // !!!: This function may return NULL
+  // TODO: we should uniformly pick nodes in a tree to avoid returning NULL
+  if (random() % 2) return root;
+
+  node_t *subnode = root->subnodes;
+  node_t *tmp = NULL;
+  node_t *ret = NULL;
+  while (subnode) {
+    tmp = subnode->next;
+    ret = _pick_node(subnode);
+    if (ret) return ret;
+    subnode = tmp;
+  }
+
   return NULL;
 }
 
@@ -10,14 +24,17 @@ tree_t *random_mutation(tree_t *tree) {
 
   // Randomly pick a node in the tree
   node_t *node = _pick_node(mutated_tree->root);
+  // TODO: we need to delete the following line, if we update `_pick_node`
+  //  function
+  if (node == NULL) node = mutated_tree->root;
   node_t *parent = node->parent;
 
   // Generate a new node
-  max_depth = random() % 15 + 1; // randomly pick a `max_depth` within [1, 15]
+  max_depth = random() % 15 + 1;  // randomly pick a `max_depth` within [1, 15]
   gen_func_t gen_func = gen_funcs[node->id];
-  node_t *replace_node = gen_func(0);
+  node_t *   replace_node = gen_func(0);
 
-  if (!parent) { // no parent, meaning that the picked node is the root node
+  if (!parent) {  // no parent, meaning that the picked node is the root node
     // Destroy the original root node
     node_free(node);
     // Simply set the new root node
--- test/test_tree_mutation.cpp
+++ test/test_tree_mutation.cpp
@@ -1,4 +1,46 @@
+#include "tree.h"
+#include "tree_mutation.h"
+#include "json_c_fuzz.h"
+
 #include "gtest/gtest.h"
 
 TEST(TreeMutationTest, RandomMutation) {
+  tree_t *tree = tree_create();
+  node_t *start = node_create(START);
+  tree->root = start;
+
+  // start -> json
+  node_t *json = node_create(JSON);
+  node_append_subnode(start, json);
+
+  // json -> element
+  node_t *element = node_create(ELEMENT);
+  node_append_subnode(json, element);
+
+  // element -> ws_1, value ("true"), ws_2 (NULL)
+  node_t *ws_1 = node_create(WS);
+  node_t *value = node_create_with_val(VALUE, "true", 4);
+  node_t *ws_2 = node_create(WS);
+  node_append_subnode(element, ws_1);
+  node_append_subnode(element, value);
+  node_append_subnode(element, ws_2);
+
+  // ws_1 -> sp1_1 (" "), ws_3 (NULL)
+  node_t *sp1_1 = node_create_with_val(SP1, " ", 1);
+  node_t *ws_3 = node_create(WS);
+  node_append_subnode(ws_1, sp1_1);
+  node_append_subnode(ws_1, ws_3);
+
+  tree_t *mutated_tree = random_mutation(tree);
+  bool    ret = tree_equal(mutated_tree, tree);
+  EXPECT_FALSE(ret);
+
+  tree_to_buf(tree);
+  fprintf(stderr, "Before: %.*s\n", (int)tree->data_len, tree->data_buf);
+  tree_to_buf(mutated_tree);
+  fprintf(stderr, "After: %.*s\n", (int)mutated_tree->data_len,
+          mutated_tree->data_buf);
+
+  tree_free(tree);
+  tree_free(mutated_tree);
 }
