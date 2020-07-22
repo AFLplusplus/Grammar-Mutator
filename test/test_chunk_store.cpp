3dfd6698f07dfd7b4d72245f93c2b7d964a40cae
h1994st <h1994st@gmail.com>
2020-07-21 23:54:31 -0400

    Update test cases for the chunk store

--- test/test_chunk_store.cpp
+++ test/test_chunk_store.cpp
@@ -30,8 +30,8 @@ TEST_F(ChunkStoreTest, SeenChunk) {
   auto node1 = node_create_with_val(1, "test", 4);
   auto node2 = node_clone(node1);
 
-  buffer node1_buffer(node1);
-  auto ret1 = seen_chunks.insert(node1_buffer);
+  // check the comparator
+  auto ret1 = seen_chunks.insert(buffer(node1));
   EXPECT_TRUE(ret1.second);
   auto ret2 = seen_chunks.insert(buffer(node2));
   EXPECT_FALSE(ret2.second);
@@ -49,7 +49,7 @@ TEST_F(ChunkStoreTest, AddNode) {
   node_set_subnode(node1, 0, node2);
 
   chunk_store_add_node(node1);
-  //  chunk_store_add_node(node1);
+  chunk_store_add_node(node1);
 
   EXPECT_EQ(seen_chunks.size(), chunk_store[1].size());
 
@@ -57,8 +57,6 @@ TEST_F(ChunkStoreTest, AddNode) {
 }
 
 TEST_F(ChunkStoreTest, AddTree) {
-  srandom(0);  // Fix the random seed
-
   auto tree = tree_create();  // "{" + "123" + "}"
   auto node1 = node_create(1);
   auto node2 = node_create_with_val(0, "{", 1);
@@ -82,7 +80,20 @@ TEST_F(ChunkStoreTest, AddTree) {
 }
 
 TEST_F(ChunkStoreTest, GetAlternativeNode) {
-  EXPECT_TRUE(true);
+  // input: nullptr, output: nullptr
+  EXPECT_EQ(chunk_store_get_alternative_node(nullptr), nullptr);
+
+  // input: a node that is not in the chunk store, output: nullptr
+  auto node = node_create(1);
+  EXPECT_EQ(chunk_store_get_alternative_node(node), nullptr);
+
+  chunk_store_add_node(node);
+  // input: a node that has been saved in the chunk store, output: the same node
+  auto _node = chunk_store_get_alternative_node(node);
+  EXPECT_TRUE(node_equal(_node, node));
+
+  node_free(node);
+  node_free(_node);
 }
 
 int main(int argc, char **argv) {
