2faaa78bf76e59f87eb59059f16cb30e97f6ee92
h1994st <h1994st@gmail.com>
2020-07-31 00:54:51 -0400

    Add a helper script to convert nautilus grammar files

---
+++ grammars/README.md
@@ -0,0 +1,6 @@
+- `nautilus_py_grammars` directory contains grammar files used in [nautilus](https://github.com/nautilus-fuzz/nautilus/tree/master/grammars)
+- `nautilus_py_grammar_to_json.py` can convert a nautilus grammar file to the JSON grammar format used in this project
+
+    ```bash
+    python nautilus_py_grammar_to_json.py /path/to/input/file /path/to/output/file
+    ```
---
+++ grammars/nautilus_py_grammar_to_json.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2020-07-30 23:09:30
+# @Author  : Shengtuo Hu
+# @Link    : https://shengtuo.me
+
+import re
+import sys
+import json
+
+OLD_KEY_RE = r'\{(?P<key>[a-zA-Z0-9_]+?)\}'
+NEW_KEY_RE = r'(?P<key><[a-zA-Z0-9_]+?>)'
+
+
+class Context(object):
+    data = {}
+
+    def __init__(self):
+        super(Context, self).__init__()
+
+    def rule(self, key, rule):
+        key = '<%s>' % (key)
+        if self.data.get(key) is None:
+            self.data[key] = []
+
+        rule = re.sub(OLD_KEY_RE, r'<\g<key>>', rule)  # key
+
+        rule = re.sub(r'\\{', '{', rule)
+        rule = re.sub(r'\\}', '}', rule)
+        rule = re.sub(r'\\\\', '\\\\', rule)
+
+        rule = re.split(NEW_KEY_RE, rule)
+        rule = list(filter(None, rule))  # remove empty strings
+        self.data[key].append(rule)
+
+
+def main():
+    if len(sys.argv) > 2:
+        input_file = sys.argv[1]
+        output_file = sys.argv[2]
+
+    ctx = Context()
+    with open(input_file, 'r') as fp:
+        exec(fp.read())
+
+    with open(output_file, 'w') as fp:
+        json.dump(ctx.data, fp, indent=4)
+
+
+if __name__ == '__main__':
+    main()
