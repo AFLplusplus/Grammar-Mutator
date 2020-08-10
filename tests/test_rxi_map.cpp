87a7222bccff6b6bde6bdbd713358d17d7437af3
h1994st <h1994st@gmail.com>
2020-08-09 23:59:54 -0400

    Finish test_rxi_map

--- tests/test_rxi_map.cpp
+++ tests/test_rxi_map.cpp
@@ -16,11 +16,53 @@ class RxiMapTest : public ::testing::Test {
   }
 
   void TearDown() override {
+    const char *key;
+    map_iter_t iter = map_iter(&list_map);
+    while ((key = map_next(&list_map, &iter))) {
+      list_free(*map_get(&list_map, key));
+    }
+
     map_deinit(&list_map);
   }
 };
 
 TEST_F(RxiMapTest, MapGet) {
+  // non-existing key
+  list_t **p_list = map_get(&list_map, "key1");
+  EXPECT_EQ(p_list, nullptr);
+
+  // add a list
+  list_t *list = list_create();
+  map_set(&list_map, "key1", list);
+
+  p_list = map_get(&list_map, "key1");
+  EXPECT_NE(p_list, nullptr);
+  EXPECT_EQ(*p_list, list);
+}
+
+TEST_F(RxiMapTest, MapSet) {
+  list_t *list = list_create();
+  EXPECT_EQ(map_set(&list_map, "key1", list), 0);
+}
+
+TEST_F(RxiMapTest, MapRemove) {
+  // should not fail
+  map_remove(&list_map, "key1");
+
+  // add a list
+  list_t *list = list_create();
+  map_set(&list_map, "key1", list);
+  list_t **p_list = map_get(&list_map, "key1");
+  EXPECT_NE(p_list, nullptr);
+  EXPECT_EQ(*p_list, list);
+
+  map_remove(&list_map, "key1");
+
+  // non-existing key
+  p_list = map_get(&list_map, "key1");
+  EXPECT_EQ(p_list, nullptr);
+
+  list_free(list);
 }
 
 int main(int argc, char **argv) {
